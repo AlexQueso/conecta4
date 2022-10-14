7a07d40c66c17e3e58023c40405496808a5752f3
Alex Quesada <a.quemen@gmail.com>
2022-10-14 19:07:34 +0200

    delete player

--- documentView/src/main/java/conecta4/models/Turn.java
+++ documentView/src/main/java/conecta4/models/Turn.java
@@ -5,20 +5,20 @@ import conecta4.types.Color;
 public class Turn {
     private Board board;
     static final int NUMBER_PLAYERS = 2;
-    private final Player[] players;
+    private final Color[] players;
     private int activePlayer;
 
     public Turn(Board board) {
         assert board != null;
 
         this.board = board;
-        players = new Player[Turn.NUMBER_PLAYERS];
+        players = new Color[Turn.NUMBER_PLAYERS];
         prepareTurn();
     }
 
     public void prepareTurn() {
         for (int i = 0; i < NUMBER_PLAYERS; i++) {
-            players[i] = new Player(Color.get(i), board);
+            players[i] = Color.get(i);
         }
         activePlayer = 0;
         board.reset();
@@ -28,7 +28,7 @@ public class Turn {
         activePlayer = (activePlayer + 1) % Turn.NUMBER_PLAYERS;
     }
     public Color getColorActivePlayer() {
-        return players[activePlayer].getColor();
+        return players[activePlayer];
     }
 
     public boolean isGameOver() {
@@ -36,7 +36,7 @@ public class Turn {
     }
 
     public boolean isConnect4() {
-        return board.isConnect4(players[activePlayer].getColor());
+        return board.isConnect4(players[activePlayer]);
     }
 
     public boolean isTie() {
