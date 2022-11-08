94f6c5304218f70532026734f766f7eefab23d4e
JesusBorrero <shyfu98@gmail.com>
2022-11-08 12:47:31 +0100

    Add Utils and types test

---
+++ modelViewPresenter/withDoubleDispatching/src/test/java/connect4/types/ColorTest.java
@@ -0,0 +1,30 @@
+package connect4.types;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ColorTest {
+
+    @Test
+    public void testGivenNewColorWhenGetThenReturn() {
+        assertThat(Color.get(0), is(Color.R));
+        assertThat(Color.get(1), is(Color.Y));
+    }
+
+    @Test
+    public void testGivenNewColorWhenGetThenAssertionError() {
+        Assertions.assertThrows(AssertionError.class, () -> Color.get(2));
+        Assertions.assertThrows(AssertionError.class, () -> Color.get(-1));
+    }
+
+    @Test
+    public void testGivenColorWhenIsNullThenReturn() {
+        assertThat(Color.R.isNull(), is(false));
+        assertThat(Color.Y.isNull(), is(false));
+        assertThat(Color.NULL.isNull(), is(true));
+    }
+
+}
